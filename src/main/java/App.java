3bc204f926b866aebbb99b2fd2fa495e211c4eff
lacrinosa <wow7222@mail.ru>
2025-01-31 20:32:14 +0500

    done

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,6 +1,8 @@
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+import java.sql.SQLOutput;
+
 public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
@@ -8,5 +10,16 @@ public class App {
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+        HelloWorld bean1 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+
+        boolean helloWorldComparison = bean == bean1;
+        System.out.println("Сравнение helloWorld bean's: " + helloWorldComparison);
+
+        Cat cat = (Cat) applicationContext.getBean("cat");
+        Cat cat1 = (Cat) applicationContext.getBean("cat");
+
+        boolean catComparison = cat == cat1;
+        System.out.println("Сравнение Cat bean's: " + catComparison);
     }
 }
